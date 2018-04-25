8e809a22a536706d65ad8901a3c5cc649c90afe4
Stehlik Lukas <lukas.stehlik@tieto.com>
2018-04-25 07:44:38 +0200

    Add OpenStack create user script

---
+++ OpenStack/oscreateuser.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+"""
+Single file script for creating OpenStack users.
+
+Single user can be created by calling script with positional arguments.
+~# oscreateuser.py jdoe doe@mail.com "John Doe"
+
+Batch of users can be created from file, with selected separator.
+Users file with `;` separator.
+user1;mail@mail1.com;John Doe
+user2;mail@mail2.com;Foo Bar
+~# oscreateuser.py -l users
+"""
+
+import sys
+import argparse
+import getpass
+import os
+import random
+import string
+from keystoneauth1 import identity
+from keystoneauth1 import session
+from keystoneclient.v3 import client as keystoneClient
+
+
+def printuser(name, email, description, password):
+    template = '''
+Hello,
+
+your OpenStack/SwiftStack account has been created.
+
+User name: {0}
+User password: {1}
+URL: 
+Domain: Default
+    '''
+    print("-----------------------------------------------------------------------------------------------------------")
+    print("Username: " + name + " e-mail: " + email + " Description: " + description + " Password: " + password)
+    print("Mail message to :" + email)
+    print(template.format(name, password))
+    print("-----------------------------------------------------------------------------------------------------------")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('username', help='Username (login)', nargs='?')
+parser.add_argument('email', help='E-mail address', nargs='?')
+parser.add_argument('description', help='Description', nargs='?')
+parser.add_argument('-deo', help='Don\'t do endpoint override', action='store_true')
+parser.add_argument('-l', help='Users list. Format username email description. Selectable separator. One entry per row.')
+args, unknown = parser.parse_known_args()
+
+try:
+    username = os.environ['OS_USERNAME']
+    password = os.environ['OS_PASSWORD']
+    project_name = os.environ['OS_PROJECT_NAME']
+    project_domain_name = os.environ['OS_USER_DOMAIN_NAME']
+    user_domain_name = os.environ['OS_USER_DOMAIN_NAME']
+    auth_url = os.environ['OS_AUTH_URL']
+except Exception as e:
+    print("Missing environment variable " + str(e) + " ! EXITING.")
+    exit(1)
+
+auth = identity.Password(auth_url=auth_url,
+                         username=username,
+                         password=password,
+                         project_name=project_name,
+                         project_domain_name=project_domain_name,
+                         user_domain_name=user_domain_name)
+sess = session.Session(auth=auth)
+
+try:
+    if args.deo:
+        keystone = keystoneClient.Client(session=sess)
+    else:
+        keystone = keystoneClient.Client(session=sess, endpoint_override=auth_url)
+except Exception as e:
+    print("Keystone client could not be initialized. EXITING \n" + str(e))
+    exit(1)
+
+if args.username and not args.l:
+    password = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+    try:
+        ks = keystone.users.create(name=args.username, email=args.email, description=args.description,
+                                   password=password)
+        printuser(args.username, args.email, args.description, password)
+    except Exception as e:
+        print("Creation of user " + args.username + " failed: " + str(e))
+elif args.l:
+    users = []
+    if os.path.exists(os.path.join(args.l)):
+        print('Using users list from file: ' + args.l)
+        list = open(args.l, mode='r')
+        sep = input('Type separator between values: ')
+        for line in list:
+            parsedline = line.strip('\n').split(sep)
+            users.append(parsedline)
+            password = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(12))
+            try:
+                ks = keystone.users.create(name=parsedline[0], email=parsedline[1], description=parsedline[2],
+                                           password=password)
+                printuser(parsedline[0], parsedline[1], parsedline[2], password)
+            except Exception as e:
+                print("Creation of user " + parsedline[0] + " failed: " + str(e))
+else:
+    print("Nothing passed to script. Use -h to print help.")
+
+
+
+
+
+
+
